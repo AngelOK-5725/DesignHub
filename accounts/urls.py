9779ea327ee7ba2d253a7b093ac0a261d6e2410f
Тамила <hamraevatamila@gmail.com>
2026-03-26 15:29:34 +0500

    Refactor URL namespacing for account-related links and enhance item usage terms in templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
+app_name = 'accounts'
 
 urlpatterns = [
     path('register/', views.register, name='register'),
